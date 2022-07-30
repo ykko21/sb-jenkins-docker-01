30f5182b885f271c3656e40b98d81d0cbcc9cf62
ykko21 <ykko21@gmail.com>
2022-07-30 19:59:05 -0400

    hello message added

--- src/main/java/org/ykko21/hello/HelloApplication.java
+++ src/main/java/org/ykko21/hello/HelloApplication.java
@@ -2,12 +2,20 @@ package org.ykko21.hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class HelloApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloApplication.class, args);
+	}
+
+	@GetMapping
+	public String hello() {
+		return "Hello!";
 	}
 
 }
